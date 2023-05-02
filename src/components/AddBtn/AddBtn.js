d59e24d796ff9e14c8f0f42b8c1eae1f505fe1cc
nastiaymlnv <nastia.yemelianova@gmail.com>
2023-05-02 17:39:37 +0300

    create add button

---
+++ src/components/AddBtn/AddBtn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { AddBtnContainer, VerticalLine, HorizontalLine } from "./AddBtn.style";
+
+const AddBtn = () => {
+    return (
+        <AddBtnContainer>
+            <VerticalLine />
+            <HorizontalLine />
+        </AddBtnContainer>
+    )
+}
+
+export default AddBtn;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import AddBtn from "../../components/AddBtn/AddBtn";
+
 const Home = () => {
     return (
-        <>
+        <div style={{height: '100vh'}}>
             <h1> Home page </h1>
-        </>
+            <AddBtn />
+        </div>
     )
 }
 
