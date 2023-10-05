adee135317eb7107a9dad0466d8319f064b5c30e
Anastasiia Yemelianova <nastia.yemelianova@gmail.com>
2023-10-05 17:17:34 +0300

    add basic filter

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -11,12 +11,19 @@ import {
     List,
     Card,
     ListItemText,
+    IconButton,
     Typography
 } from "@mui/material";
+import FilterListIcon from '@mui/icons-material/FilterList';
+import ListSubheader from '@mui/material/ListSubheader';
+import ListItemButton from '@mui/material/ListItemButton';
+
 
 import { AddBtn } from "../../components";
 
 import { monthNames } from "./dateItemsNames";
+// move to enums
+import { operationTypes } from "../CreateOperation/operationTypes";
 
 import css from "./Home.module.css";
 
@@ -24,30 +31,72 @@ const Home = ({ setShowBottomNav }) => {
     const navigate = useNavigate();
     const transactions = useSelector(state => state.operations);
     const balance = useSelector(state => state.balance);
+    const [isFilterOpen, setIsFilterOpen] = useState(false);
+    const [filteredTransactions, setFilteredTransactions] = useState(transactions);
     setShowBottomNav(true);
     const generalBalance = [...Object.values(balance)].reduce((accum, curr) => accum += curr, 0);
+
+    useEffect(() => {
+        setFilteredTransactions(transactions);
+    }, [transactions]);
 
     const handleItemClick = (transactionId) =>
         navigate(`/operation/${transactionId}`);
 
+    const handleFilter = () => {
+        setIsFilterOpen(!isFilterOpen);
+    }
+
+    const handleFilterItem = (selectedType) => {
+        setIsFilterOpen(false);
+        if (selectedType !== "Clear") setFilteredTransactions(transactions.filter(item => item.type === selectedType));
+        else setFilteredTransactions(transactions);
+    }
+
     return (
         <>
             <AppBar position="static">
-                <Toolbar>
+                <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
                     <Box>
                         <Typography variant="body2">
                             Balance
                         </Typography>
-                        <Typography sx={{fontWeight: "bold"}}>
-                            { generalBalance } UAH
+                        <Typography sx={{ fontWeight: "bold" }}>
+                            {generalBalance} UAH
                         </Typography>
                     </Box>
-                    {/* add filter */}
+                    <IconButton color="inherit" onClick={handleFilter}>
+                        <FilterListIcon />
+                    </IconButton>
                 </Toolbar>
             </AppBar>
+            {isFilterOpen &&
+                <List
+                    sx={{ position: "absolute", zIndex: 1, right: 0, minWidth: 250, bgcolor: 'background.paper', boxShadow: 3, borderRadius: "5px" }}
+                    component="nav"
+                    subheader={
+                        <ListSubheader component="div">
+                            Sort by
+                        </ListSubheader>
+                    }
+                >
+                    {operationTypes.map((item, index) => {
+                        return <ListItemButton key={index} onClick={() => handleFilterItem(item)} >
+                            <Typography variant="body1">
+                                {item}
+                            </Typography>
+                        </ListItemButton>
+                    })}
+                    <ListItemButton onClick={() => handleFilterItem('Clear')} >
+                        <Typography variant="body1">
+                            Clear all filters
+                        </Typography>
+                    </ListItemButton>
+                </List>
+            }
             <Box sx={{ height: "100vh" }} className={css["Home-content-container"]}>
                 <List className={css["Operation-list"]}>
-                    {transactions.map((item) => {
+                    {filteredTransactions && filteredTransactions.map((item) => {
                         const time = new Date(item.date);
                         const day = time.getDate();
                         const month = monthNames[time.getMonth()];
@@ -92,7 +141,7 @@ const Home = ({ setShowBottomNav }) => {
                                                 : css["Operation-list__item-price_green"]
                                         )}
                                     >
-                                        {item.type === "Income"? item.amount : -item.amount} UAH
+                                        {item.type === "Income" ? item.amount : -item.amount} UAH
                                     </div>
                                     <div className={css["Operation-list__item-date"]}>
                                         {day} {month} {year}
